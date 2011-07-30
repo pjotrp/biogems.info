11e18e13ce56590b91b5c235fa5262ea5db54da0
Pjotr Prins <pjotr.public01@thebird.nl>
2011-07-30 10:34:18 +0200

    Add links

--- website/src/helpers/content_helper.rb
+++ website/src/helpers/content_helper.rb
@@ -3,15 +3,22 @@ require 'yaml'
 module ContentHelper
   def by_popularity
     spec = YAML::load(File.new("./etc/bio-projects.yaml").read)
+    sorted = spec.sort { |a, b| b[1][:downloads] <=> a[1][:downloads] }
     i = 0
     dl = 0
     descr = 'unknown'
-    spec.each_key do | name |
+    sorted.each do | rec |
+      name = rec[0]
       i += 1
       plugin = spec[name]
       descr = plugin[:summary]
       descr = plugin[:description] if !descr
-      yield i,plugin[:downloads],name,normalize(descr),plugin[:authors].join(', ')
+      # docs
+      docs = plugin[:homepage]   # from the biogem descr
+      docs = plugin[:homepage_uri] if !docs
+      docs = plugin[:project_uri] if !docs
+      docs = plugin[:source_code_uri] if !docs
+      yield i,plugin[:downloads].to_i,name,normalize(descr),plugin[:authors].join(', '),docs
     end
   end
 
