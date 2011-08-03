52b20e85598db36325a05150bee0e0d8b7ff74f2
Pjotr Prins <pjotr.public01@thebird.nl>
2011-08-03 10:19:35 +0200

    Add RSS feed

---
+++ bin/rss.rb
@@ -0,0 +1,37 @@
+# Generate RSS feed
+#
+
+require 'yaml'
+require 'rss/maker'
+
+version = "2.0" # ["0.9", "1.0", "2.0"]
+destination = "rss.xml" # local file to write
+
+content = RSS::Maker.make(version) do |m|
+  m.channel.title = "biogems.info"
+  m.channel.link = "http://biogems.info/rss.html"
+  m.channel.description = "Ruby for bioinformatics"
+  m.items.do_sort = true # sort items by date
+
+  spec = YAML::load(File.new("./var/bio-projects.yaml").read)
+  spec.each do | rec |
+    rec[1][:release_date].to_s =~ /^(\d\d\d\d)\-(\d\d)\-(\d\d)/
+    t = Time.new($1.to_i,$2.to_i,$3.to_i)
+    rec[1][:time] = t
+  end
+
+  sorted = spec.sort { |a, b| b[1][:time] <=> a[1][:time] }
+
+  sorted.each_with_index do | rec, i |
+    name = rec[0]
+    plugin = rec[1]
+    rss = m.items.new_item
+    rss.title = "#{name} #{plugin[:version]} released"
+    rss.link = "http://biogems.info/##{name}"
+    # p plugin[:time]
+    rss.date = plugin[:time]
+    break if i > 12
+  end
+end
+
+print content
--- create_website.sh
+++ create_website.sh
@@ -5,5 +5,9 @@
 # Download stats
 ruby bin/list-bio.rb > ./var/bio-projects.yaml 
 
+# RSS
+ruby bin/rss.rb > website/site/rss.xml
+
 # Generate site into website/site/
 staticmatic build website/
+
