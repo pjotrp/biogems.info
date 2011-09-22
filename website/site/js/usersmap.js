0b5118e233a3f5955d57c1e2a787ee3f762b157e
Raoul J.P. Bonnal <ilpuccio.febo@gmail.com>
2011-09-22 11:56:08 +0200

    add prototype page UsersMap, using OpenLayer

---
+++ website/site/js/usersmap.js
@@ -0,0 +1,34 @@
+var map, layer;
+
+function init(){
+    OpenLayers.ProxyHost="/proxy/?url=";
+    map = new OpenLayers.Map('map');
+    layer = new OpenLayers.Layer.WMS( "OpenLayers WMS", 
+        "http://vmap0.tiles.osgeo.org/wms/vmap0", {layers: 'basic'} );
+        
+    map.addLayer(layer);
+    map.setCenter(new OpenLayers.LonLat(0, 0), 0);
+
+    var newl = new OpenLayers.Layer.Text( "text", { location:"./js/OpenLayer/textfile.txt"} );
+    map.addLayer(newl);
+
+    var markers = new OpenLayers.Layer.Markers( "Markers" );
+    map.addLayer(markers);
+
+    var size = new OpenLayers.Size(21,25);
+    var offset = new OpenLayers.Pixel(-(size.w/2), -size.h);
+    var icon = new OpenLayers.Icon('http://www.openlayers.org/dev/img/marker.png',size,offset);
+    markers.addMarker(new OpenLayers.Marker(new OpenLayers.LonLat(5.73788,50.92818),icon));
+
+    var halfIcon = icon.clone();
+    markers.addMarker(new OpenLayers.Marker(new OpenLayers.LonLat(0,45),halfIcon));
+
+    marker = new OpenLayers.Marker(new OpenLayers.LonLat(90,10),icon.clone());
+    marker.setOpacity(0.2);
+    marker.events.register('mousedown', marker, function(evt) { alert(this.icon.url); OpenLayers.Event.stop(evt); });
+    markers.addMarker(marker); 
+    map.addControl(new OpenLayers.Control.LayerSwitcher());
+    map.zoomToMaxExtent();
+
+    halfIcon.setOpacity(0.5);
+}
