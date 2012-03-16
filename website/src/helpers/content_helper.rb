3556a0b9a8da7043e680a168b1448711f8619a62
Pjotr Prins <pjotr.public01@thebird.nl>
2012-03-16 09:15:04 +0100

    Override travis image for project

--- website/src/helpers/content_helper.rb
+++ website/src/helpers/content_helper.rb
@@ -62,7 +62,11 @@ module ContentHelper
       user = plugin[:github_user]
       project = plugin[:github_project]
       test_info[:url]   = "http://travis-ci.org/#!/#{user}/#{project}"
-      test_info[:image] = "https://secure.travis-ci.org/#{user}/#{project}.png?branch=master"
+      if plugin[:travis_img]
+        test_info[:image] = plugin[:travis_img]
+      else
+        test_info[:image] = "https://secure.travis-ci.org/#{user}/#{project}.png?branch=master"
+      end
 
       yield i+1,plugin[:downloads90],plugin[:downloads],name,plugin[:status],version,released,normalize(descr),cite,plugin[:authors].join(', '),home,docs,src,issues,num_issues,test_info,commit,trend_direction,rank90[name]
     end
