234c93e9d30aad3d892e2fee9c50c34c389cf56a
Pjotr Prins <pjotr.public01@thebird.nl>
2012-03-26 08:45:24 -0400

    Tell openssl to find certificates

--- bin/list-bio.rb
+++ bin/list-bio.rb
@@ -68,10 +68,13 @@ end
 def get_https_body url
   uri = URI.parse(url)
   $stderr.print "Fetching #{url}\n"
-  http = Net::HTTP.new(uri.host, 443)
-  http.use_ssl = true
+  https = Net::HTTP.new(uri.host, 443)
+  https.use_ssl = true
+  https.verify_mode = OpenSSL::SSL::VERIFY_PEER
+  https.ca_path = '/etc/ssl/certs' if File.exists?('/etc/ssl/certs') # Ubuntu
+  https.ca_file = '/opt/local/share/curl/curl-ca-bundle.crt' if File.exists?('/opt/local/share/curl/curl-ca-bundle.crt') # Mac OS X
   request = Net::HTTP::Get.new(uri.request_uri)
-  response = http.request(request)
+  response = https.request(request)
   if response.code.to_i != 200
     $stderr.print "get_http_body not found for "+url
     return "{}"
