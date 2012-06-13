6fb279264c8090524f099107a30b6b72a4bbafa8
Pjotr Prins <pjotr.public01@thebird.nl>
2012-06-13 08:31:29 +0200

    Cucumber test passes for blogs

--- features/rss-feature.rb
+++ features/rss-feature.rb
@@ -8,13 +8,13 @@ Given /^I have one rss blog feed configured$/ do
 end
 
 When /^I parse the rss feed$/ do
-  @rss_feed = generate_biogems_rss_feed "test/data/rss/bio-projects.yaml", "test/data/rss/rss-blogs.yaml"
+  @rss_feed = generate_biogems_rss_feed "test/data/bio-projects.yaml", "test/data/rss/rss-blogs.yaml"
 end
 
 Then /^the resulting rss feed should have the same contents as the rss feed$/ do
   titles = @rss_feed.items.map { |item| item.title }
-  titles[0].should == "RSS test title 1 (RSS blog)"
-  titles[1].should == "RSS test title 2 (RSS blog)"
+  titles.index("RSS test title 1 (RSS blog)").should be_true
+  titles.index("RSS test title 2 (RSS blog)").should be_true
 end
 
 Given /^I have one atom blog feed configured$/ do
@@ -23,12 +23,12 @@ Given /^I have one atom blog feed configured$/ do
 end
 
 When /^I parse the atom feed$/ do
-  @atom_feed = generate_biogems_rss_feed "test/data/rss/bio-projects.yaml", "test/data/rss/atom-blogs.yaml"
+  @atom_feed = generate_biogems_rss_feed "test/data/bio-projects.yaml", "test/data/rss/atom-blogs.yaml"
 end
 
 Then /^the resulting rss feed should have the same contents as the atom feed$/ do
   titles = @atom_feed.items.map { |item| item.title }
-  titles[0].should == "Atom test title 1 (Atom blog)"
-  titles[1].should == "Atom test title 2 (Atom blog)"
+  titles.index("Atom test title 1 (Atom blog)").should be_true
+  titles.index("Atom test title 2 (Atom blog)").should be_true
 end
 
