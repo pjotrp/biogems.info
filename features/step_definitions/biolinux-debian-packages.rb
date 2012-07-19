9850a0757e356a1fa71b5773cd0a1bd66795372f
Pjotr Prins <pjotr.public01@thebird.nl>
2012-07-19 16:23:25 +0200

    Adding steps

---
+++ features/step_definitions/biolinux-debian-packages.rb
@@ -0,0 +1,41 @@
+Given /^I have a Debian package description in a BioLinux Manifest$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^I fetch Debian package info for "(.*?)"$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I should get the name "(.*?)" and description "(.*?)"$/ do |arg1, arg2|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I should get the popularity count of (\d+)$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^I check the Bio Med information$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^it should check it is a Bio Med package$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^it should check it is not a Bio Med package$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^it should check it is a BioLinux package$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^I should get the popularity count of (\d+)$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^it should check it is a science package$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+
