b28be4c6c35b68f966c63884d403c71dd3a7ac7b
Pjotr Prins <pjotr.public01@thebird.nl>
2012-07-20 10:12:32 +0200

    -test switch added

--- bin/create-biolinux-db.rb
+++ bin/create-biolinux-db.rb
@@ -7,17 +7,29 @@ $: << "lib"
 require 'biogems/biolinux/biolinux'
 require 'biogems/debian/debian'
 
-@biolinux = BiolinuxManifest.new(`curl -s https://raw.github.com/chapmanb/cloudbiolinux/master/manifest/debian-packages.yaml`)
+is_testing = ARGV[0] == '--test'
+
+BIOLINUX_DEBIAN_MANIFEST = 
+  if is_testing
+    File.read('test/data/biolinux/debian-packages.yaml')
+  else
+    `curl -s https://raw.github.com/chapmanb/cloudbiolinux/master/manifest/debian-packages.yaml` 
+end
+
+@biolinux = BiolinuxManifest.new(BIOLINUX_DEBIAN_MANIFEST)
 
 # Debian Tasks
 tasknames = 'bio bio-dev bio-ngs bio-phylogeny cloud data epi his oncology'
 biomed = []
 
-tasknames.split.each do |taskname|
-  $stderr.print "Fetching ", taskname,"\n"
-  biomed << Debian::BlendTask.new(`curl -s http://anonscm.debian.org/viewvc/blends/projects/med/trunk/debian-med/tasks/#{taskname}?view=co`)
+if is_testing
+  biomed << Debian::BlendTask.new(File.read('test/data/debian/bio-task.txt'))
+else
+  tasknames.split.each do |taskname|
+    $stderr.print "Fetching ", taskname,"\n"
+    biomed << Debian::BlendTask.new(`curl -s http://anonscm.debian.org/viewvc/blends/projects/med/trunk/debian-med/tasks/#{taskname}?view=co`)
+  end
 end
-
 pkgs = []
 @biolinux.each do | name, pkg |
   biomed.each do |bm|
