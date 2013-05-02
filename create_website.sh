320a4c6c4f4202aa235134aaeb976da19156e9a7
Pjotr Prins <pjotr.public01@thebird.nl>
2013-05-02 10:23:49 +0200

    Reinstating stuff

---
+++ create_website.sh
@@ -0,0 +1,19 @@
+#! /bin/sh
+#
+# Create the website
+
+# Download stats
+curl https://api.github.com/rate_limit
+
+# curl http://github.com/api/v2/json/issues/list/pjotrp/bioruby-affy/open
+./bin/fetch-geminfo.rb $* > ./var/bio-projects.yaml 
+./bin/fetch-geminfo.rb --rubygems > ./var/ruby-projects.yaml
+# bundle exec ./bin/fetch-geminfo.rb > ./var/bio-projects.yaml 
+
+# Create RSS feed for others to use
+bundle exec ./bin/rss.rb > website/site/rss.xml
+
+# Generate site into website/site/
+staticmatic build website/
+
+curl https://api.github.com/rate_limit
