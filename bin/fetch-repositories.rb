6b1ea28f5147940236c4d37202701c90759d8eec
Pjotr Prins <pjotr.public01@thebird.nl>
2014-06-02 13:45:20 +0200

    Stats

--- bin/fetch-repositories.rb
+++ bin/fetch-repositories.rb
@@ -1,5 +1,11 @@
 #! /usr/bin/env ruby
 
+require 'yaml'
+
 yfile = ARGV.shift
 
 print "Fetching git repositories from #{yfile} with Ruby #{RUBY_VERSION}\n"
+
+geminfo = YAML::load(yfile)
+
+p geminfo[0]
