54c8e5b2ddc2709a17d85c734e8962b306aab74a
pprins <pjotr.public01@thebird.nl>
2014-06-30 10:08:24 +0200

    Mkdir

--- bin/fetch-repositories.rb
+++ bin/fetch-repositories.rb
@@ -11,11 +11,13 @@ geminfo = YAML::load(File.read(yfile))
 geminfo.each do | name, info | 
   user = info[:github_user]
   project = info[:github_project]
-  p [name, user, project]
+  p [name, user, project, info[:source_code_uri]]
   if project
     dir = 'data/repositories/'+project
-    p dir
-    # Dir.mkdir(dir)
+    if not File.exist?(dir)
+      p dir
+      Dir.mkdir(dir)
+    end
   else
     $stderr.print "WARNING: no project defined for #{name}!\n"
   end
