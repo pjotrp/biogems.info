c81197623207e33c4872379361e4cf89c8faa567
Pjotr Prins <pjotr.public01@thebird.nl>
2015-10-24 11:06:12 +0200

    Output gem list

--- bin/fetch-gemlist.rb
+++ bin/fetch-gemlist.rb
@@ -16,12 +16,33 @@ is_testing = ARGV.index('--test')
 
 $stderr.print "Get all rubygems starting with bio- (bio dash)\n"
 if is_testing
-  list = ['bio','bio-blastxmlparser']
+  list = ['bio','bio-blastxmlparser','bio-publisci']
 else
   list = `gem search bio-`.split(/\n/)
   list = list.map { |item| item.split(" ")[0] }
 end
 
+h = {}
+list.each { |n| h[n] = nil }
+                             
+$stderr.print "Get information from YAML files in ./etc/biogems/\n"
+list2 = Dir.glob("./etc/biogems/*.yaml")
+list2.each do | yamlfn |
+  gem = File.basename(yamlfn,".yaml")
+  # p gem
+  info = YAML.load(File.read(yamlfn))
+  if info[:status] == 'disabled'
+    h.delete(gem)
+  else
+    h[gem] = info
+  end
+end
 
+raise "gem should not be there" if h['bio-publisci']
 
-print list.to_json  # output to STDOUT
+if is_testing
+  list = list[0..3]
+end
+
+$stderr.print "Output YAML\n"
+print h.to_yaml  # output to STDOUT
